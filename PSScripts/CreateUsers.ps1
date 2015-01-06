bbdd0606123de9ed0b0d0a5ce99e46fecf6377be
David Worthington <david.worthington@outlook.com>
2015-01-06 16:35:27 +0100

    Added Script for creating test users

---
+++ PSScripts/CreateUsers.ps1
@@ -0,0 +1,14 @@
+﻿$passwd = Read-Host "Enter NAV User password" -AsSecureString
+$instance = Get-NAVServerInstance
+
+for ($i=0; $i -le 10;$i++)
+{
+    $TestUserName = 'User' +$i
+    New-NAVServerUser $instance.ServerInstance -UserName $TestUserName -Password $passwd -Verbose -LicenseType Full
+    New-NAVServerUserPermissionSet $instance.ServerInstance -UserName $TestUserName -PermissionSetId SUPER -Verbose
+}
+
+Get-NAVServerUser $instance.ServerInstance 
+Get-NAVServerUserPermissionSet $instance.ServerInstance 
+
+
